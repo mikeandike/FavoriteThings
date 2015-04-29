0b2adf49739b1d667cb85bd2774f835084226317
Michael Sacks <michaelsacks@gmail.com>
2015-04-29 11:21:48 -0600

    Basic Table View

---
+++ FavoriteThings-Objc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FavoriteThings-Objc
+//
+//  Created by Michael Sacks on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FavoriteThings-Objc/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  FavoriteThings-Objc
+//
+//  Created by Michael Sacks on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (strong, nonatomic) UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
+    [self.view addSubview:self.tableView];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
